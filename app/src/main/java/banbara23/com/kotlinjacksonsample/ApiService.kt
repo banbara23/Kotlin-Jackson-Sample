70bca5d06993bfeac05e72e069a2b71e51f33bdf
ikemura <k_ikemura@yumemi.co.jp>
2018-01-31 17:06:07 +0900

    convert ApiService to kotlin

---
+++ app/src/main/java/banbara23/com/kotlinjacksonsample/ApiService.kt
@@ -0,0 +1,13 @@
+package banbara23.com.kotlinjacksonsample
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("/")
+    fun post(): Observable<PostResponse>
+
+    @GET("/")
+    fun get(): Observable<GetResponse>
+}
